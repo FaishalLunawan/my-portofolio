5fc577583c7f48240793020e29a66c4bf6e2fed5
FaishalLunawan <faishallunawan@gmail.com>
2025-12-02 10:38:33 +0700

    setup: konfigurasi deployment GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,18 @@
+// GANTI ISI FILE svelte.config.js DENGAN INI:
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/my-portofolio' : ''
-		}
+			fallback: 'index.html', // PENTING untuk SPA
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
